1558829b630503eb19b35dfc19002864ba27769a
DayiGorbay <dayigorbay4224@gmail.com>
2026-02-16 13:13:27 +0330

    add cli_table package in console result, add show error in console ui, add persian and english (LTR and RTL) version, add status message in console ui, add exit process.

--- bin/currency_api.dart
+++ bin/currency_api.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
 import 'package:currency_api/Controllers/api.dart' as api;
+import 'dart:io';
+import 'package:cli_table/cli_table.dart';
 
 String formatNumber(String value) {
   final number = int.parse(value);
@@ -8,20 +11,79 @@ String formatNumber(String value) {
   );
 }
 
+void clearConsole() {
+  if (Platform.isWindows) {
+    Process.runSync('cls', [], runInShell: true);
+  } else {
+    Process.runSync('clear', [], runInShell: true);
+  }
+}
+
 Future<void> main (List<String> arguments) async {
   // print('Currency API Project: ${api.getResponse()}!');
 
-  await api.getResponse();
+  final table_en = Table(  
+    header: ['Name', 'English Name', 'Price', 'Chaneg Vlue', 'Change Percent', 'Last Update'],
+    columnWidths: [15,20,15,15,15,20],
+  );
+
+  final table_fa = Table(
+    header: ['آخرین بروزرسانی', 'درصد تغییر', 'مقدار تغییر', 'قیمت ارز', 'نام لاتین', 'نام ارز'],
+    columnWidths: [20,15,15,15,20,15],
+  );
+
+  print("fetching data from API...");
+
+  String result = await api.getResponse();
+
+  if (result != "Success") {
+    print("Error: $result");
+    print("Press enter to exit...");
+    await stdin.first;
+    return;
+  }
+
+  print("Done!");
+  sleep(Duration(seconds: 1));
+  clearConsole();
 
   for (var item in api.currency) {
 
-    print("نام: ${item.name}");
-    print("${item.name_en} :نام لاتین");
-    print("قیمت: ${formatNumber(item.price as String)} ${item.unit}");
-    print("مقدار تغییر: ${formatNumber(item.change_value as String)} | درصد تغییر: %${item.change_percent}");
-    print("آخرین بروزرسانی قیمت: ${item.date} ${item.time}");
-    print("-------------------");
-    
+    // print("نام: ${item.name}");
+    // print("${item.name_en} :نام لاتین");
+    // print("قیمت: ${formatNumber(item.price as String)} ${item.unit}");
+    // print("مقدار تغییر: ${formatNumber(item.change_value as String)} | درصد تغییر: %${item.change_percent}");
+    // print("آخرین بروزرسانی قیمت: ${item.date} ${item.time}");
+    // print("-------------------");
+
+    // English alignment version
+    table_en
+      .add([
+        item.name,
+        item.name_en,
+        "${formatNumber(item.price as String)} ${item.unit}",
+        formatNumber(item.change_value as String),
+        "${item.change_percent}%",
+        "${item.date} - ${item.time}",
+      ]);
+
+    // Persian alignment version
+    table_fa
+      .add([
+        "${item.date} - ${item.time}",
+        "${item.change_percent}%",
+        formatNumber(item.change_value as String),
+        "${formatNumber(item.price as String)} ${item.unit}",
+        item.name_en,
+        item.name,
+      ]);
+
   }
 
-}
+  print(table_en.toString());
+  print(table_fa.toString());
+
+  print("Press enter to exit...");
+  await stdin.first;
+
+} /***************** CLI Version *****************/
--- lib/Controllers/api.dart
+++ lib/Controllers/api.dart
@@ -2,52 +2,66 @@ import 'package:currency_api/Models/currency.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 import 'package:dotenv/dotenv.dart';
+import 'dart:io';
 
 List<Currency> currency = [];
 
 final env = DotEnv();
 
-Future<void> getResponse() async {
+Future<String> getResponse() async {
 
-  env.load();
+  try {
 
-  var url = Uri.parse('${env['API_URL']}?key=${env['API_KEY']}');
+    env.load();
 
-  final response = await http.get(url);
+    var url = Uri.parse('${env['API_URL']}?key=${env['API_KEY']}');
 
-  if (response.statusCode == 200) {
+    final response = await http.get(url);
 
-    Map<String, dynamic> jsonResponse = convert.jsonDecode(response.body);
+    if (response.statusCode == 200) {
 
-    List currencyList = jsonResponse["currency"];
+      Map<String, dynamic> jsonResponse = convert.jsonDecode(response.body);
 
-    if (currencyList.isNotEmpty) {
+      List currencyList = jsonResponse["currency"];
 
-      currency.clear();
+      if (currencyList.isNotEmpty) {
 
-      for (var item in currencyList) {
+        currency.clear();
 
-        currency.add(
+        for (var item in currencyList) {
 
-          Currency(
+          currency.add(
+            Currency(
+              date: item['date'].toString(),
+              time: item['time'].toString(),
+              name_en: item['name_en'].toString(),
+              name: item['name'].toString(),
+              price: item['price'].toString(),
+              change_value: item['change_value'].toString(),
+              change_percent: item['change_percent'].toString(),
+              unit: item['unit'].toString(),
+            ),
+          );
 
-            date: item['date'].toString(),
-            time: item['time'].toString(),
-            name_en: item['name_en'].toString(),
-            name: item['name'].toString(),
-            price: item['price'].toString(),
-            change_value: item['change_value'].toString(),
-            change_percent: item['change_percent'].toString(),
-            unit: item['unit'].toString(),
-            
-          ),
-
-        );
-
+        }
       }
+
+      return "Success";
+
+    } else {
+
+      return "HTTP ${response.statusCode}";
 
     }
 
-  }
+  } on SocketException {
 
+    return "No Internet Connection";
+
+  } catch (e) {
+
+    return "Unexpected Error";
+
+  }
+  
 }
